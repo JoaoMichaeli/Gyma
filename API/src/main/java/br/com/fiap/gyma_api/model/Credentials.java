e9a4ba84e49708a3c9061f478d2c4bacef53a126
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-05-15 11:42:06 -0300

    removendo a api

--- API/src/main/java/br/com/fiap/gyma_api/model/Credentials.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.gyma_api.model;
-
-public record Credentials(
-        String email,
-        String password
-) {
-}
--- API/src/main/java/br/com/fiap/gyma_api/model/Token.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.gyma_api.model;
-
-public record Token(
-        String token,
-        String email
-) {
-}
